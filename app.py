bd2f0875474ca0711c6f390498b9b1f1c37040f0
CapybaraHome <lyaminlosha@yandex.ru>
2025-12-15 01:07:36 +0300

    добавлены исполнители и crud к ним

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ from datetime import datetime
 import sqlite3
 app = Flask(__name__)
 
+class Act:
+    act_data = sqlite3.Row
+    act_performers = sqlite3.Row
+
+
 @app.route('/')
 def events():
     data_base = sqlite3.connect('events.db')
@@ -54,23 +59,41 @@ def event_detail(id):
                                  ''', (id,))
     current_event_type = cursor.fetchone()
 
-    event_types = cursor.execute(''' SELECT * FROM event_type ''')
+    cursor.execute(''' SELECT * FROM event_type ''')
     event_types = cursor.fetchall()
 
     cursor.execute('''
             SELECT act.id as act_id, 
                    act.name as act_name, 
                    act.number as act_number, 
-                   act.performer as act_performer,
-                   act_type_id as act_type_id,
+                   act.act_type_id as act_type_id,
                    act_type.name as act_type_name
             FROM act
             JOIN act_type ON act.act_type_id = act_type.id
             WHERE (event_id == ?) AND (deleted_at IS NULL) ORDER BY act.number
             ''', (id,))
-    acts = cursor.fetchall()
+    acts_data = cursor.fetchall()
+    performers_data = []
+    for act in acts_data:
+        cursor.execute('''  SELECT *
+                            FROM performer_act
+                            JOIN performer ON performer_act.performer_id = performer.id
+                            WHERE act_id is ?
+                       ''',( act['act_id'],))
+        performers_data.append(cursor.fetchall())
     data_base.close()
-    return render_template('acts/event_acts_list.html', event=event, acts=acts, event_types=event_types, current_event_type=current_event_type,  event_id=id)
+
+    acts = []
+    for act in acts_data:
+        acts.append([act])
+    for a in acts:
+        a.append(performers_data[acts.index(a)])
+    return render_template('acts/event_acts_list.html',
+                           event=event,
+                           acts=acts,
+                           event_types=event_types,
+                           current_event_type=current_event_type,
+                           event_id=id)
 
 @app.route('/add_event/', methods=['GET', 'POST'])
 def add_event():
@@ -177,11 +200,28 @@ def add_act(id):
 
         created_at = today
         updated_at = today
-        cursor.execute('''
-            INSERT INTO act (number, name, performer, created_at, updated_at, event_id)
-            VALUES (?, ?, ?, ?, ?, ?)''', (number, name, performer, created_at, updated_at, id))
-        data_base.commit()
-        data_base.close()
+        if request.method == 'POST':
+            cursor.execute('''
+                           UPDATE act
+                           SET number      = ?,
+                               name        = ?,
+                               updated_at  = ?,
+                               act_type_id = ?
+                           WHERE id = ?
+                           ''', (request.form.get('number'),
+                                 request.form.get('name'),
+                                 datetime.now(),
+                                 request.form.get('act_type_id'),
+                                 id))
+            if current_performers:
+                for p_id in p_ids:
+                    cursor.execute('''
+                                   INSERT INTO performer_act (act_id, performer_id)
+                                   VALUES (?, ?) ON CONFLICT (act_id, performer_id) DO NOTHING
+                                   ''', (id, p_id))
+            data_base.commit()
+            data_base.close()
+            return redirect(url_for('event_detail', id=act['event_id']))
         return redirect(url_for('event_detail', id=id))
     return render_template('acts/add_act.html')
 
@@ -189,11 +229,24 @@ def add_act(id):
 def act_detail(id):
     data_base = sqlite3.connect('events.db')
     cursor = data_base.cursor()
+
     cursor.row_factory = sqlite3.Row
     cursor.execute('''
         SELECT * FROM act WHERE id is ? AND event_id is NOT NULL
         ''', (id,))
     act = cursor.fetchone()
+    cursor.execute('''
+    SELECT * from performer
+    ''')
+    performers = cursor.fetchall()
+
+    p_ids = list(int(i) for i in request.form.getlist('performer_ids[]'))
+    placeholders = ','.join('?' * len(p_ids))
+    sql = f'''
+                   SELECT * from performer WHERE id in ({placeholders})
+                   '''
+    cursor.execute(sql, p_ids)
+    current_performers = cursor.fetchall()
 
     cursor.execute('''
         SELECT * FROM act_type
@@ -206,18 +259,38 @@ def act_detail(id):
     current_act_type = cursor.fetchone()
 
     if request.method == 'POST':
+
+        p_ids = list(int(i) for i in request.form.getlist('performer_ids[]'))
+
+        placeholders = ','.join('?' * len(p_ids))
+        sql = f'''
+                SELECT * from performer WHERE id in ({placeholders})
+                '''
+        cursor.execute(sql, p_ids)
+        current_performers = cursor.fetchall()
+
         cursor.execute('''
-            UPDATE act SET number = ?, name = ?, performer = ?, updated_at = ?, act_type_id = ? WHERE id = ?
+            UPDATE act SET number = ?, name = ?, updated_at = ?, act_type_id = ? WHERE id = ?
             ''', (request.form.get('number'),
                   request.form.get('name'),
-                  request.form.get('performer'),
                   datetime.now(),
                   request.form.get('act_type_id'),
                   id))
+        if current_performers:
+            for p_id in p_ids:
+                cursor.execute('''
+                    INSERT INTO performer_act (act_id, performer_id) VALUES (?, ?)
+                    ''', (id, p_id))
         data_base.commit()
         data_base.close()
         return redirect(url_for('event_detail', id=act['event_id']))
-    return render_template('acts/add_act.html', act=act, act_types=act_types, current_act_type=current_act_type, args=request.args)
+    return render_template('acts/add_act.html',
+                           act=act,
+                           act_types=act_types,
+                           current_act_type=current_act_type,
+                           args=request.args,
+                           performers=performers,
+                           current_performers=current_performers)
 
 @app.route('/delete_act/<int:id>', methods=['GET', 'POST'])
 def delete_act(id):
@@ -401,6 +474,68 @@ def delete_act_type(id):
     data_base.close()
     return redirect(url_for('act_type_list'))
 
+
+@app.route('/performer_list/')
+def performer_list():
+
+    data_base = sqlite3.connect('events.db')
+    cursor = data_base.cursor()
+    cursor.row_factory = sqlite3.Row
+    cursor.execute('''
+        SELECT * FROM performer
+        ''')
+    performers = cursor.fetchall()
+    data_base.close()
+    return render_template('performers/performer_list.html', performers=performers)
+
+@app.route('/add_performer/', methods=['GET', 'POST'])
+def add_performer():
+    data_base = sqlite3.connect('events.db')
+    cursor = data_base.cursor()
+    cursor.row_factory = sqlite3.Row
+
+    if request.method == 'POST':
+
+        cursor.execute('''
+                       INSERT INTO performer (first_name, last_name, middle_name, location_city)
+                       VALUES (?, ?, ?, ?)
+                       ''', (request.form.get('first_name'), request.form.get('last_name'), request.form.get('middle_name'), request.form.get('location_city')))
+        data_base.commit()
+        data_base.close()
+        return redirect(url_for('performer_list'))
+    data_base.close()
+    return render_template('performers/add_performer.html')
+
+@app.route('/performer_detail/<int:id>', methods=['GET', 'POST'])
+def performer_detail(id):
+    data_base = sqlite3.connect('events.db')
+    cursor = data_base.cursor()
+    cursor.row_factory = sqlite3.Row
+    cursor.execute('''
+        SELECT * FROM performer WHERE id = ?
+        ''', (id,))
+    performer = cursor.fetchone()
+    if request.method == 'POST' and performer is not None:
+        cursor.execute('''
+            UPDATE performer SET first_name = ?, last_name = ?, middle_name = ?, location_city = ? WHERE id = ?
+            ''', (request.form.get('first_name'), request.form.get('last_name'), request.form.get('middle_name'), request.form.get('location_city'), id))
+        data_base.commit()
+        data_base.close()
+        return redirect(url_for('performer_list'))
+    data_base.close()
+    return render_template('performers/add_performer.html', performer=performer)
+
+@app.route('/delete_performer/<int:id>', methods=['GET', 'POST'])
+def delete_performer(id):
+    data_base = sqlite3.connect('events.db')
+    cursor = data_base.cursor()
+    cursor.row_factory = sqlite3.Row
+    cursor.execute('''
+        DELETE FROM performer WHERE id = ?
+        ''', (id,))
+    data_base.commit()
+    data_base.close()
+    return redirect(url_for('performer_list'))
 # НЕ СТИРАТЬ
 if __name__ == '__main__':
     app.run(debug=True)
